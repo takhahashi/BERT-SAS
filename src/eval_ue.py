bf57c7d983d14cafabd9745dc00003d955b1f7a5
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-06-29 20:33:07 +0900

    None

--- src/eval_ue.py
+++ src/eval_ue.py
@@ -15,7 +15,7 @@ from transformers import AutoTokenizer
 from utils.utils_data import TrainDataModule
 from utils.cfunctions import simple_collate_fn
 from utils.utils_models import create_module
-from utils.dataset import get_Dataset
+from utils.dataset import get_Dataset, get_upper_score
 from utils.ue_metric_func import calc_rcc_auc, calc_rpp, calc_roc_auc, calc_risk
 from models.functions import return_predresults
 from ue4nlp.ue_estimater_ensemble import UeEstimatorEnsemble
@@ -23,86 +23,207 @@ from ue4nlp.ue_estimater_trust import UeEstimatorTrustscore
 from ue4nlp.ue_estimater_mcd import UeEstimatorDp
 
 
-@hydra.main(config_path="/content/drive/MyDrive/GoogleColab/1.AES/ASAP/test1/configs", config_name="eval_config")
+@hydra.main(config_path="/content/drive/MyDrive/GoogleColab/SA/ShortAnswer/BERT-SAS/configs", config_name="eval_ue_config")
 def main(cfg: DictConfig):
-    train_dataset = get_Dataset(cfg.model.reg_or_class, 
-                               cfg.path.traindata_file_name, 
-                               cfg.aes.prompt_id, 
-                               AutoTokenizer.from_pretrained(cfg.model.model_name_or_path),
-                               )
-    test_dataset = get_Dataset(cfg.model.reg_or_class, 
-                               cfg.path.testdata_file_name, 
-                               cfg.aes.prompt_id, 
-                               AutoTokenizer.from_pretrained(cfg.model.model_name_or_path),
-                               )
-    
-    if cfg.eval.collate_fn == True:
-        collate_fn = simple_collate_fn
-    else:
-        collate_fn = None
+    five_fold_results = []
+    for fold in range(5):
+        with open('/content/drive/MyDrive/GoogleColab//SA/ShortAnswer/Y15/1_5_results/Reg_{}/fold{}'.format(cfg.sas.score_id, fold)) as f:
+            fold_results = json.load(f)
+        five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
 
-    train_dataloader = torch.utils.data.DataLoader(train_dataset,
-                                                  batch_size=cfg.eval.batch_size,
-                                                  shuffle=False,
-                                                  collate_fn=collate_fn,
-                                                  )
-    test_dataloader = torch.utils.data.DataLoader(test_dataset,
-                                                  batch_size=cfg.eval.batch_size,
-                                                  shuffle=False,
-                                                  collate_fn=collate_fn,
-                                                  )
+    upper_score = get_upper_score(cfg.sas.question_id, cfg.sas.score_id)
+    save_dir_path = cfg.path.save_dir_path
 
-    low, high = get_score_range(cfg.aes.prompt_id)
-    model = create_module(cfg.model.model_name_or_path, 
-                          cfg.model.reg_or_class, 
-                          learning_rate=1e-5, 
-                          num_labels=high-low+1, 
-                          )
-    model.load_state_dict(torch.load(cfg.path.model_save_path))
-
-    eval_results = return_predresults(model, test_dataloader, rt_clsvec=False, dropout=False)
-
-    softmax = nn.Softmax(dim=1)
-    probs = softmax(torch.tensor(eval_results['logits']))
-    max_prob = probs[torch.arange(len(probs)), torch.argmax(probs, dim=-1)]
-    eval_results.update({'MP': max_prob.numpy().copy()})
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##simple var####
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = foldr['score']
+        uncertainty = foldr['calib_var']
+        risk = calc_risk(pred, true, 'reg', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='reg', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/simplevar'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
 
 
-    trust_estimater = UeEstimatorTrustscore(model, 
-                                            train_dataloader, 
-                                            cfg.aes.prompt_id,
-                                            cfg.model.reg_or_class,
-                                            )
-    trust_estimater.fit_ue()
-    trust_results = trust_estimater(test_dataloader)
-    eval_results.update(trust_results)
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], [] 
+    ##reg_trust_score####
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = foldr['score']
+        uncertainty = -foldr['trust_score']
+        risk = calc_risk(pred, true, 'reg', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='reg', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/reg_trust_score'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
 
-    
 
-    mcdp_estimater = UeEstimatorDp(model,
-                                   cfg.ue.num_dropout,
-                                   cfg.model.reg_or_class,
-                                   )
-    mcdp_results = mcdp_estimater(test_dataloader)
-    eval_results.update(mcdp_results)
-    
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##reg_dp###
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = foldr['score']
+        uncertainty = foldr['calib_mcdp_var']
+        risk = calc_risk(pred, true, 'reg', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='reg', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/reg_dp'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
 
 
-    ensemble_estimater = UeEstimatorEnsemble(model, 
-                                             cfg.ue.ensemble_model_paths,
-                                             cfg.model.reg_or_class,
-                                             )
-    ensemble_results = ensemble_estimater(test_dataloader)
-    eval_results.update(ensemble_results)
 
-    list_results = {}
-    for k, v in eval_results.items():
-        if type(v) == type(np.array([1, 2.])):
-            list_results.update({k: v.tolist()})
-        else:
-            list_results.update({k: v})
-    with open(cfg.path.results_save_path, mode="wt", encoding="utf-8") as f:
-        json.dump(list_results, f, ensure_ascii=False)
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##reg_ense###
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = foldr['score']
+        uncertainty = foldr['calib_ense_var']
+        risk = calc_risk(pred, true, 'reg', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='reg', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/reg_mul'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
+
+    ##class###
+    five_fold_results = []
+    for fold in range(5):
+        with open('/content/drive/MyDrive/GoogleColab//SA/ShortAnswer/Y15/1_5_results/Class_{}/fold{}'.format(cfg.sas.score_id, fold)) as f:
+            fold_results = json.load(f)
+        five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+
+
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##MP####
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = np.argmax(foldr['logits'], axis=-1)
+        uncertainty = -foldr['MP']
+        risk = calc_risk(pred, true, 'class', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='class', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/MP'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
+
+
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##class trust####
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = np.argmax(foldr['logits'], axis=-1)
+        uncertainty = -foldr['trust_score']
+        risk = calc_risk(pred, true, 'class', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='class', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/class_trust_score'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
+
+
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##class dp####
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = foldr['mcdp_score'] 
+        uncertainty = foldr['mcdp_uncertainty']
+        risk = calc_risk(pred, true, 'class', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='class', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/class_dp'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
+
+
+    fresults_rcc, fresults_rpp, fresults_roc, fresults_rcc_y = [], [], [], []
+    ##class mul####
+    for foldr in five_fold_results:
+        true = foldr['labels']
+        pred = foldr['ense_score']
+        uncertainty = foldr['ense_uncertainty'] 
+        risk = calc_risk(pred, true, 'class', upper_score, binary=True)
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
+        rpp = calc_rpp(conf=-uncertainty, risk=risk)
+        roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, reg_or_class='class', upper_score=upper_score)
+        fresults_rcc = np.append(fresults_rcc, rcc_auc)
+        fresults_rcc_y.append(rcc_y)
+        fresults_roc = np.append(fresults_roc, roc_auc)
+        fresults_rpp = np.append(fresults_rpp, rpp)
+    results_dic = {'rcc': np.mean(fresults_rcc), 
+                   'rpp': np.mean(fresults_rpp), 
+                   'roc': np.mean(fresults_roc), 
+                   'rcc_y': fresults_rcc_y}
+    save_path = save_dir_path + '/class_mul'
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
     
 
 if __name__ == "__main__":
