b043c9d05100aa1d6bc8a62425d7dec3fa6e9f72
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-06-29 11:04:56 +0900

    None

--- src/eval_ue.py
+++ src/eval_ue.py
@@ -25,48 +25,88 @@ from ue4nlp.ue_estimater_mcd import UeEstimatorDp
 
 @hydra.main(config_path="/content/drive/MyDrive/GoogleColab/1.AES/ASAP/test1/configs", config_name="eval_config")
 def main(cfg: DictConfig):
-    five_fold_results = []
-    for fold in range(5):
-        with open('/content/drive/MyDrive/GoogleColab/1.AES/ASAP/Reg-torchlightning/pt{}/fold_{}/pred_results'.format(cfg.aes.prompt_id, fold)) as f:
-            fold_results = json.load(f)
-        five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+    train_dataset = get_Dataset(cfg.model.reg_or_class, 
+                               cfg.path.traindata_file_name, 
+                               cfg.aes.prompt_id, 
+                               AutoTokenizer.from_pretrained(cfg.model.model_name_or_path),
+                               )
+    test_dataset = get_Dataset(cfg.model.reg_or_class, 
+                               cfg.path.testdata_file_name, 
+                               cfg.aes.prompt_id, 
+                               AutoTokenizer.from_pretrained(cfg.model.model_name_or_path),
+                               )
+    
+    if cfg.eval.collate_fn == True:
+        collate_fn = simple_collate_fn
+    else:
+        collate_fn = None
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                                  batch_size=cfg.eval.batch_size,
+                                                  shuffle=False,
+                                                  collate_fn=collate_fn,
+                                                  )
+    test_dataloader = torch.utils.data.DataLoader(test_dataset,
+                                                  batch_size=cfg.eval.batch_size,
+                                                  shuffle=False,
+                                                  collate_fn=collate_fn,
+                                                  )
+
+    low, high = get_score_range(cfg.aes.prompt_id)
+    model = create_module(cfg.model.model_name_or_path, 
+                          cfg.model.reg_or_class, 
+                          learning_rate=1e-5, 
+                          num_labels=high-low+1, 
+                          )
+    model.load_state_dict(torch.load(cfg.path.model_save_path))
+
+    eval_results = return_predresults(model, test_dataloader, rt_clsvec=False, dropout=False)
+
+    softmax = nn.Softmax(dim=1)
+    probs = softmax(torch.tensor(eval_results['logits']))
+    max_prob = probs[torch.arange(len(probs)), torch.argmax(probs, dim=-1)]
+    eval_results.update({'MP': max_prob.numpy().copy()})
+
+
+    trust_estimater = UeEstimatorTrustscore(model, 
+                                            train_dataloader, 
+                                            cfg.aes.prompt_id,
+                                            cfg.model.reg_or_class,
+                                            )
+    trust_estimater.fit_ue()
+    trust_results = trust_estimater(test_dataloader)
+    eval_results.update(trust_results)
 
     
-    results_dic = {}
-    for ue_type in ['logvar', 'trust_score', 'mcdp_uncertainty', 'ense_uncertainty']: 
-        five_fold_roc, five_fold_rcc, five_fold_rcc_y, five_fold_rpp = [], [], [], []
-        for fold_result in five_fold_results:
-            true = fold_result['labels']
-            if ue_type == 'logvar':
-                pred = fold_result['score']
-                uncertainty = fold_result['calib_var']
-            elif ue_type == 'mcdp_uncertainty':
-                pred = fold_result['mcdp_score']
-                uncertainty = fold_result['calib_mcdp_var']
-            elif ue_type == 'ense_uncertainty':
-                pred = fold_result['ense_score']
-                uncertainty = fold_result['calib_ense_var']
-            else:
-                pred = fold_result['score']
-                uncertainty = -fold_result['trust_score']
 
-            risk = calc_risk(pred, true, cfg.aes.prompt_id, binary=cfg.ue.binary_risk)
-            rcc_auc, rcc_x, rcc_y = calc_rcc_auc(conf=-uncertainty, risk=risk)
-            rpp = calc_rpp(conf=-uncertainty, risk=risk)
-            roc_auc = calc_roc_auc(pred, true, conf=-uncertainty, prompt_id=cfg.aes.prompt_id)
+    mcdp_estimater = UeEstimatorDp(model,
+                                   cfg.ue.num_dropout,
+                                   cfg.model.reg_or_class,
+                                   )
+    mcdp_results = mcdp_estimater(test_dataloader)
+    eval_results.update(mcdp_results)
+    
 
-            five_fold_rcc = np.append(five_fold_rcc, rcc_auc)
-            five_fold_rpp = np.append(five_fold_rpp, rpp)
-            five_fold_roc = np.append(five_fold_roc, roc_auc)
-            five_fold_rcc_y.append(rcc_y)
-        results_dic.update({ue_type: {'rcc': np.mean(five_fold_rcc).tolist(),
-                                      'rpp': np.mean(five_fold_rcc).tolist(),
-                                      'roc': np.mean(five_fold_roc).tolist(),
-                                      'rcc_y': np.mean(np.array(five_fold_rcc_y), axis=0).tolist()}})
-        
-        
+
+    ensemble_estimater = UeEstimatorEnsemble(model, 
+                                             cfg.ue.ensemble_model_paths,
+                                             cfg.model.reg_or_class,
+                                             )
+    ensemble_results = ensemble_estimater(test_dataloader)
+    eval_results.update(ensemble_results)
+
+    list_results = {}
+    for k, v in eval_results.items():
+        if type(v) == type(np.array([1, 2.])):
+            list_results.update({k: v.tolist()})
+        else:
+            list_results.update({k: v})
     with open(cfg.path.results_save_path, mode="wt", encoding="utf-8") as f:
-        json.dump(results_dic, f, ensure_ascii=False)
+        json.dump(list_results, f, ensure_ascii=False)
+    
+
+if __name__ == "__main__":
+    main()
     
 
 if __name__ == "__main__":
