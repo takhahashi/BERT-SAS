7d224aae21ac16adef763b7df42ed93c0d4941a8
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-09-22 11:41:23 +0900

    None

--- src/mix_test.py
+++ src/mix_test.py
@@ -14,7 +14,7 @@ from utils.cfunctions import simple_collate_fn, EarlyStopping
 from utils.utils_models import create_module
 from models.functions import return_predresults
 from utils.cfunctions import regvarloss
-from models.models import Scaler, Bert, Reg_class_mixmodel
+from models.models import Scaler, Bert, Reg_class_mixmodel, EscoreScaler
 from ue4nlp.ue_estimater_trust import UeEstimatorTrustscore
 from ue4nlp.ue_estimater_ensemble import UeEstimatorEnsemble
 from ue4nlp.ue_estimater_trust import UeEstimatorTrustscore
@@ -25,12 +25,12 @@ import json
 
 @hydra.main(config_path="/content/drive/MyDrive/GoogleColab/SA/ShortAnswer/BERT-SAS/configs", config_name="eval_mix")
 def main(cfg: DictConfig):
-    if cfg.model.inftype == 'expected_score':
-        expected_score = True
-        save_path_str = cfg.path.results_save_path + '_expected_score'
+    if cfg.model.inftype == 'exp_score':
+        save_path_str = cfg.path.results_save_path + '_exp_score'
+    elif cfg.model.inftype == 'weighted_exp_score':
+        save_path_str = cfg.path.results_save_path + '_weighted_exp_score'
     else:
-        expected_score = False
-        save_path_str = cfg.path.resutls_save_path
+        save_path_str = cfg.path.results_save_path
     tokenizer = AutoTokenizer.from_pretrained(cfg.model.model_name_or_path)
     upper_score = get_upper_score(cfg.sas.question_id, cfg.sas.score_id)
 
@@ -57,11 +57,18 @@ def main(cfg: DictConfig):
     model = Reg_class_mixmodel(bert, num_classes=upper_score+1)
     model.load_state_dict(torch.load(cfg.path.model_save_path))
     eval_results = return_predresults(model, test_dataloader, rt_clsvec=False, dropout=False)
-    if expected_score == True:
+    if cfg.model.inftype == 'exp_score':
         reg_pred = np.round(eval_results['score'] * upper_score)
         class_pred = np.argmax(eval_results['logits'], axis=-1)
         expected_pred = ((reg_pred + class_pred)/2.) / upper_score
         eval_results['score'] = expected_pred
+    elif cfg.model.inftype == 'eweighted_exp_score':
+        e_scaler = EscoreScaler(init_S=1.0)
+        e_scaler.load_state_dict(torch.load(cfg.path.scaler_savepath))
+        reg_pred = torch.tensor(np.round(eval_results['score'] * upper_score))
+        class_pred = torch.tensor(np.argmax(eval_results['logits'], axis=-1).astype('int32'))
+        weighted_exp_score = e_scaler.left(class_pred).detach().numpy() + e_scaler.right(reg_pred).detach().numpy()
+        eval_results['score'] = weighted_exp_score / upper_score
 
     softmax = nn.Softmax(dim=1)
     pred_int_score = torch.tensor(np.round(eval_results['score'] * upper_score), dtype=torch.int32)
@@ -69,6 +76,7 @@ def main(cfg: DictConfig):
     mix_trust = pred_probs[torch.arange(len(pred_probs)), pred_int_score]
     eval_results.update({'mix_conf': mix_trust.numpy().copy()})
 
+    """
     mcdp_estimater = UeEstimatorDp(model,
                                    cfg.ue.num_dropout,
                                    cfg.model.reg_or_class,
@@ -86,7 +94,6 @@ def main(cfg: DictConfig):
     ensemble_results = ensemble_estimater(test_dataloader, expected_score=expected_score)
     eval_results.update(ensemble_results)
 
-    """
     max_prob = pred_probs[torch.arange(len(pred_probs)), torch.argmax(pred_probs, dim=-1)]
     eval_results.update({'MP': max_prob.numpy().copy()})
 
