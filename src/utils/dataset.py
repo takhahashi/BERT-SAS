c8e957cebd801e51df649852b518cc4aef864a63
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-09-27 22:17:01 +0900

    None

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -39,12 +39,44 @@ upper_score_dic = {
       'C_Score': 3, 
       'D_Score': 4,
       'E_Score': 2
+  },  
+  '1-2_1_3': { 
+      'A_Score': 2,
+      'B_Score': 5,
+      'C_Score': 3, 
+      'D_Score': 6,
+  },
+  '1-2_2_4': { 
+      'A_Score': 3,
+      'B_Score': 2,
+      'C_Score': 4, 
+      'D_Score': 3,
+  },
+  '2-1_1_5': { 
+      'A_Score': 2,
+      'B_Score': 7,
+      'C_Score': 6, 
+  },
+  '2-1_2_3': { 
+      'A_Score': 3,
+      'B_Score': 4,
+      'C_Score': 3, 
+      'D_Score': 2,
+  },
+  '2-2_1_4': { 
+      'A_Score': 6,
+      'B_Score': 3,
+      'C_Score': 6, 
+  },
+  '2-2_2_3': { 
+      'A_Score': 6,
+      'B_Score': 6,
+      'C_Score': 2, 
   }
 }
 
 def get_upper_score(q_id, s_id):
      return upper_score_dic[q_id][s_id]
-
 
 def get_dataset(dataf, s_id, upper_s, inftype, tokenizer):
   text, score = [], []
