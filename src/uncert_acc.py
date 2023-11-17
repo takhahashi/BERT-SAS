29b385e9b84c4ebc44503467fbf5135ae392fedd
takhahashi <yuu1719m@ezweb.ne.jp>
2023-11-17 16:21:13 +0900

    None

---
+++ src/uncert_acc.py
@@ -0,0 +1,142 @@
+import os
+
+import json
+import hydra
+import numpy as np
+from omegaconf import DictConfig
+from utils.dataset import get_upper_score
+from utils.ue_metric_func import calc_rcc_auc, calc_rpp
+from sklearn.metrics import roc_auc_score
+
+@hydra.main(config_path="/content/drive/MyDrive/GoogleColab/SA/ShortAnswer/BERT-SAS/configs", config_name="eval_ue_config")
+def main(cfg: DictConfig):
+    model_type = cfg.model.model_type
+    uncert_type = cfg.ue.uncert_type
+    save_dir_path = cfg.path.save_dir_path
+    upper_score = get_upper_score(cfg.sas.question_id, cfg.sas.score_id)
+    five_fold_model_outputs = load_five_fold_results(cfg.sas.prompt_id, cfg.sas.question_id, cfg.sas.score_id, model_type)
+    five_fold_trues, five_fold_preds, five_fold_uncert = extract_true_pred_uncert(five_fold_model_outputs, model_type, uncert_type, upper_score)
+
+    five_fold_rpp, five_fold_roc, five_fold_rcc, five_fold_rcc_y = [], [], [], []
+    for true, pred, uncert in zip(five_fold_trues, five_fold_preds, five_fold_uncert):
+        if true[0] != np.int32 and pred[0] != np.int32:
+            raise ValueError(f'`{type(true[0])}` is not valid type')
+        five_fold_rpp = np.append(five_fold_rpp, calc_rpp(true, pred, uncert))
+        five_fold_roc = np.append(five_fold_roc, roc_auc_score(true == pred, -uncert))
+        rcc_auc, rcc_x, rcc_y = calc_rcc_auc(true, pred, -uncert, 'scaledrmse', upper_score)
+        five_fold_rcc = np.append(five_fold_rcc, rcc_auc)
+        five_fold_rcc_y.append(rcc_y)
+    results_dic = {'rcc': np.mean(five_fold_rcc),
+                   'rpp': np.mean(five_fold_rpp),
+                   'roc': np.mean(five_fold_roc),
+                   'rcc_y': calc_mean_rcc_y(five_fold_rcc_y)}
+
+    save_path = save_dir_path + '/' + model_type + '_' + uncert_type
+    with open(save_path, mode="wt", encoding="utf-8") as f:
+        json.dump(results_dic, f, ensure_ascii=False)
+    
+def calc_mean_rcc_y(rcc_y_lis):
+    min_len = len(rcc_y_lis[0])
+    for rcc_y in rcc_y_lis:
+        if len(rcc_y) < min_len:
+            min_len = len(rcc_y)
+    rcc_y_arr = []
+    for rcc_y in rcc_y_lis:
+        rcc_y_arr.append(np.array(rcc_y)[:min_len])
+    mean_rcc_y = np.mean(rcc_y_arr, axis=0)
+    return mean_rcc_y.tolist()
+
+def load_five_fold_results(prompt_id, question_id, score_id, model_type):
+    five_fold_results = []
+    if model_type == 'reg' or model_type == 'mul_reg':
+        for fold in range(5):
+            with open('/content/drive/MyDrive/GoogleColab//SA/ShortAnswer/{}/{}_results/Reg_{}/fold{}'.format(prompt_id, question_id, score_id, fold)) as f:
+                fold_results = json.load(f)
+            five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+    elif model_type == 'class' or model_type == 'mul_class':
+        five_fold_results = []
+        for fold in range(5):
+            with open('/content/drive/MyDrive/GoogleColab//SA/ShortAnswer/{}/{}_results/Class_{}/fold{}'.format(prompt_id, question_id, score_id, fold)) as f:
+                fold_results = json.load(f)
+            five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+    elif model_type == 'mix' or model_type == 'mul_mix':
+        five_fold_results = []
+        for fold in range(5):
+            with open('/content/drive/MyDrive/GoogleColab//SA/ShortAnswer/{}/{}_results/Mix_{}/fold{}'.format(prompt_id, question_id, score_id, fold)) as f:
+                fold_results = json.load(f)
+            five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+    elif model_type == 'gp':
+        five_fold_results = []
+        for fold in range(5):
+            with open('/content/drive/MyDrive/GoogleColab//SA/ShortAnswer/{}/{}_results/gp_{}/fold{}'.format(prompt_id, question_id, score_id, fold)) as f:
+                fold_results = json.load(f)
+            five_fold_results.append({k: np.array(v) for k, v in fold_results.items()})
+    else:
+        raise ValueError(f'`{model_type}` is not valid')
+
+    return five_fold_results
+
+def extract_true_pred_uncert(five_fold_results, model_type, uncert_type, upper_score):
+    trues, preds, uncerts = [], [], []
+    for fold_result in five_fold_results:
+        if model_type == 'reg':
+            pred = np.round(fold_result['score'] * upper_score).astype('int32')
+            true = np.round(fold_result['labels'] * upper_score).astype('int32')
+            if uncert_type == 'default':
+                uncert = fold_result['logvar']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        elif model_type == 'mul_reg':
+            pred = np.round(fold_result['ense_score'] * upper_score).astype('int32')
+            true = np.round(fold_result['labels'] * upper_score).astype('int32')
+            if uncert_type == 'default':
+                uncert = fold_result['ense_var']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        elif model_type == 'class':
+            pred = np.argmax(fold_result['logits'], axis=-1).astype('int32')
+            true = fold_result['labels'].astype('int32')
+            if uncert_type == 'default':
+                uncert = -fold_result['MP']
+            elif uncert_type == 'trust':
+                uncert = -fold_result['trust_score']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        elif model_type == 'mul_class':
+            pred = fold_result['ense_score'].astype('int32')
+            true = fold_result['labels'].astype('int32')
+            if uncert_type == 'default':
+                uncert = -fold_result['ense_MP']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        elif model_type == 'mix':
+            pred = np.round(fold_result['labels'] * upper_score).astype('int32')
+            true = np.round(fold_result['score'] * upper_score).astype('int32')
+            if uncert_type == 'default':
+                uncert = fold_result['mix_conf']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        elif model_type == 'mul_mix':
+            true = np.round(fold_result['labels'] * upper_score).astype('int32')
+            pred = np.round(fold_result['ense_score'] * upper_score).astype('int32')
+            if uncert_type == 'default':
+                uncert = fold_result['ense_MP']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        elif model_type == 'gp':
+            true = np.round(fold_result['labels']).astype('int32')
+            pred = fold_result['score'].astype('int32')
+            if uncert_type == 'default':
+                uncert = fold_result['std']
+            else:
+                raise ValueError(f'`{uncert_type}` is not valid')
+        else:
+            raise ValueError(f'`{model_type}` is not valid')
+        uncerts.append(uncert)
+        trues.append(true)
+        preds.append(pred)
+    return trues, preds, uncerts
+
+
+if __name__ == "__main__":
+    main()
--- src/utils/ue_metric_func.py
+++ src/utils/ue_metric_func.py
@@ -1,19 +1,19 @@
 import numpy as np
 from sklearn.metrics import roc_auc_score
 
-def calc_rcc_auc(true, pred, conf, metric_type, upper_score, reg_or_class=None, binary_risk=None):
+def calc_rcc_auc(true, pred, conf, metric_type, upper_score=None):
+  if true[0] != np.int32 and pred[0] != np.int32:
+    raise ValueError(f'`{type(true[0])}` is not valid type')
   if metric_type == 'normal':
-    risk = calc_risk(pred, true, reg_or_class, upper_score, binary=binary_risk)
-    auc, points_x, points_y = calc_rcc_auc_simple(conf, risk)
+    risk = (true != pred).astype('int32')
   elif metric_type == 'rmse':
-    auc, points_x, points_y = calc_rcc_auc_rmse(pred, true, conf, upper_score, reg_or_class)
+    risk = (pred - true) ** 2
   elif metric_type == 'scaledrmse':
-    auc, points_x, points_y = calc_rcc_auc_scaledrmse(pred, true, conf, upper_score, reg_or_class)
+    pred_scaled = pred/upper_score
+    true_scaled = true/upper_score
+    risk = (pred_scaled - true_scaled) ** 2
   else:
     raise ValueError("{} is not a valid value for metric_type".format(metric_type))
-  return auc, points_x, points_y
-
-def calc_rcc_auc_simple(conf, risk):
   n = len(conf)
   cr_pair = list(zip(conf, risk))
   cr_pair.sort(key=lambda x: x[0], reverse=True)
@@ -28,14 +28,28 @@ def calc_rcc_auc_simple(conf, risk):
     points_y.append(cumulative_risk[k] / (1+k))  # current avg. risk
   return auc, points_x, points_y
 
-def calc_rcc_auc_rmse(pred, true, conf, upper_score, reg_or_class):
-  if reg_or_class == 'reg':
-    pred_org = np.round(pred * upper_score).astype('int32')
-    true_org = np.round(true * upper_score).astype('int32')
-  else:
-    pred_org = pred.astype('int32')
-    true_org = true.astype('int32')
-  risk = (pred_org - true_org) ** 2
+def calc_rcc_auc_simple(true, pred, conf):
+  if true[0] != np.int32 and pred[0] != np.int32:
+    raise ValueError(f'`{type(true[0])}` is not valid type')
+  risk = (true != pred).astype('int32')
+  n = len(conf)
+  cr_pair = list(zip(conf, risk))
+  cr_pair.sort(key=lambda x: x[0], reverse=True)
+  cumulative_risk = [cr_pair[0][1]]
+  for i in range(1, n):
+    cumulative_risk.append(cr_pair[i][1] + cumulative_risk[-1])
+  points_x, points_y = [], []
+  auc = 0
+  for k in range(n):
+    auc += cumulative_risk[k] / (1+k)
+    points_x.append((1+k) / n)  # coverage
+    points_y.append(cumulative_risk[k] / (1+k))  # current avg. risk
+  return auc, points_x, points_y
+
+def calc_rcc_auc_rmse(pred, true, conf):
+  if true[0] != np.int32 and pred[0] != np.int32:
+    raise ValueError(f'`{type(true[0])}` is not valid type')
+  risk = (pred - true) ** 2
   n = len(conf)
   cr_pair = list(zip(conf, risk))
   cr_pair.sort(key=lambda x: x[0], reverse=True)
@@ -78,11 +92,14 @@ def calc_rcc_auc_scaledrmse(pred, true, conf, upper_score, reg_or_class):
     points_y.append(cumulative_risk[k] / (1+k))  # current avg. risk
   return auc, points_x, points_y
 
-def calc_rpp(conf, risk):
+def calc_rpp(true: np.ndarray, pred: np.ndarray, uncert: np.ndarray):
+  if true[0] != np.int32 and pred[0] != np.int32:
+    raise ValueError(f'`{type(true[0])}` is not valid type')
+  risk = (true != pred).astype('int32')
+  conf = -uncert
   n = len(conf)
   cr_pair = list(zip(conf, risk))
   cr_pair.sort(key=lambda x: x[0], reverse=False)
-
   cnt = 0
   for i in range(n):
     for j in range(i, n):
