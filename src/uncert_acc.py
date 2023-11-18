0ccb23edb63c33224615800e0f23504131ba824d
takhahashi <yuu1719m@ezweb.ne.jp>
2023-11-18 13:17:09 +0900

    None

--- src/uncert_acc.py
+++ src/uncert_acc.py
@@ -116,14 +116,14 @@ def extract_true_pred_uncert(five_fold_results, model_type, uncert_type, upper_s
             pred = np.round(fold_result['labels'] * upper_score).astype('int32')
             true = np.round(fold_result['score'] * upper_score).astype('int32')
             if uncert_type == 'default':
-                uncert = fold_result['mix_conf']
+                uncert = -fold_result['mix_conf']
             else:
                 raise ValueError(f'`{uncert_type}` is not valid')
         elif model_type == 'mul_mix':
             true = np.round(fold_result['labels'] * upper_score).astype('int32')
             pred = np.round(fold_result['ense_score'] * upper_score).astype('int32')
             if uncert_type == 'default':
-                uncert = fold_result['ense_MP']
+                uncert = -fold_result['ense_MP']
             else:
                 raise ValueError(f'`{uncert_type}` is not valid')
         elif model_type == 'gp':
