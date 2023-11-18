15d1532b8802d5e5c2a1475cd7b4922dfc430e52
takhahashi <yuu1719m@ezweb.ne.jp>
2023-11-18 16:12:30 +0900

    None

--- src/GP_train.py
+++ src/GP_train.py
@@ -15,62 +15,58 @@ import json
 
 @hydra.main(config_path="/content/drive/MyDrive/GoogleColab/SA/ShortAnswer/BERT-SAS/configs", config_name="GP_train")
 def main(cfg: DictConfig):
-    for s_id in ['A_Score', 'B_Score', 'C_Score', 'D_Score', 'E_Score']:
-        for fold in range(0, 5):
-            cfg.sas.score_id = s_id
-            cfg.training.fold = fold
+    tokenizer = AutoTokenizer.from_pretrained(cfg.scoring_model.model_name_or_path)
+    upper_score = get_upper_score(cfg.sas.question_id, cfg.sas.score_id)
+    with open(cfg.path.traindata_file_name) as f:
+        train_dataf = json.load(f)
+    train_dataset = get_dataset(train_dataf, cfg.sas.score_id, upper_score, cfg.scoring_model.reg_or_class, tokenizer)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, 
+                                                    batch_size=16, 
+                                                    shuffle=True, 
+                                                    drop_last=False, 
+                                                    collate_fn=simple_collate_fn)
 
-            tokenizer = AutoTokenizer.from_pretrained(cfg.scoring_model.model_name_or_path)
-            upper_score = get_upper_score(cfg.sas.question_id, cfg.sas.score_id)
 
+    num_labels = upper_score + 1
+    classifier = create_module(cfg.scoring_model.model_name_or_path,
+                        cfg.scoring_model.reg_or_class,
+                        1e-5,
+                        num_labels=num_labels,
+                        )
+    classifier = classifier.cuda()
+    classifier.load_state_dict(torch.load(cfg.path.scoring_model_savepath), strict=False)
+    classifier.eval()
 
-            with open(cfg.path.traindata_file_name) as f:
-                train_dataf = json.load(f)
-            train_dataset = get_dataset(train_dataf, cfg.sas.score_id, upper_score, cfg.scoring_model.reg_or_class, tokenizer)
-            train_dataloader = torch.utils.data.DataLoader(train_dataset, 
-                                                            batch_size=16, 
-                                                            shuffle=True, 
-                                                            drop_last=False, 
-                                                            collate_fn=simple_collate_fn)
-            
+    word_vec, labels = extract_clsvec_truelabels(classifier, train_dataloader)
+    train_x = torch.FloatTensor(word_vec)
+    train_y = torch.FloatTensor(labels)
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = GPModel(train_x, train_y, likelihood)
+    training_iter = cfg.training.iter_num
+    model.train()
+    likelihood.train()
+    model.covar_module.base_kernel.lengthscale = np.linalg.norm(train_x[0].numpy() - train_x[1].numpy().T) ** 2 / 2
 
-            num_labels = upper_score + 1
-            classifier = create_module(cfg.scoring_model.model_name_or_path,
-                                cfg.scoring_model.reg_or_class,
-                                1e-5,
-                                num_labels=num_labels,
-                                )
-            classifier = classifier.cuda()
-            classifier.load_state_dict(torch.load(cfg.path.scoring_model_savepath), strict=False)
-            classifier.eval()
+    # Use the adam optimizer
+    optimizer = torch.optim.Adam([
+        {'params': model.parameters()},  # Includes GaussianLikelihood parameters
+    ], lr=cfg.training.lr)
 
-            word_vec, labels = extract_clsvec_truelabels(classifier, train_dataloader)
-            train_x = torch.FloatTensor(word_vec)
-            train_y = torch.FloatTensor(labels)
-            likelihood = gpytorch.likelihoods.GaussianLikelihood()
-            model = GPModel(train_x, train_y, likelihood)
-            training_iter = cfg.training.iter_num
-            model.train()
-            likelihood.train()
-            model.covar_module.base_kernel.lengthscale = np.linalg.norm(train_x[0].numpy() - train_x[1].numpy().T) ** 2 / 2
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
 
-            # Use the adam optimizer
-            optimizer = torch.optim.Adam([
-                {'params': model.parameters()},  # Includes GaussianLikelihood parameters
-            ], lr=cfg.training.lr)
+    for i in range(training_iter):
+        optimizer.zero_grad()
+        output = model(train_x)
+        loss = -mll(output, train_y)
+        loss.backward()
+        print('Iter %d/%d - Loss: %.3f lengthscale: %.3f noise: %.3f' % (
+            i+1, training_iter, loss.item(),
+            model.covar_module.base_kernel.lengthscale.item(),
+            model.likelihood.noise.item()
+        ))
+        optimizer.step()
 
-            mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+    torch.save(model.state_dict(), cfg.path.save_path)
 
-            for i in range(training_iter):
-                optimizer.zero_grad()
-                output = model(train_x)
-                loss = -mll(output, train_y)
-                loss.backward()
-                print('Iter %d/%d - Loss: %.3f lengthscale: %.3f noise: %.3f' % (
-                    i+1, training_iter, loss.item(),
-                    model.covar_module.base_kernel.lengthscale.item(),
-                    model.likelihood.noise.item()
-                ))
-                optimizer.step()
-
-            torch.save(model.state_dict(), cfg.path.save_path)
+if __name__ == "__main__":
+    main()
