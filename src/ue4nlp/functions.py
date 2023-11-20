6c8173edfe6f5df24d7c4098137afa34f5a94cef
takhahashi <125870494+takhahashi@users.noreply.github.com>
2023-11-20 19:16:35 +0900

    None

--- src/ue4nlp/functions.py
+++ src/ue4nlp/functions.py
@@ -5,32 +5,27 @@ def sep_features_by_class(X_features, scores):
     sep_features = {}
     for label in np.sort(np.unique(scores)):
         sep_features['{}'.format(label)] = X_features[scores == label]
-    print(sep_features)
     return sep_features
 
 def diffclass_euclid_dist(target_feature, target_label, train_features_labels):
     min_dist = None
-    cnt = 0
     for k, v in train_features_labels.items():
         if int(k) != target_label:
             for diff_vec in v:
-                cnt += 1
                 dist = np.linalg.norm(diff_vec-target_feature)
                 if(min_dist is None or dist < min_dist):
                     min_dist = dist
-    return min_dist, cnt
+    return min_dist
 
 def sameclass_euclid_dist(target_feature, target_label, train_features_labels):
     min_dist = None
-    cnt = 0
     for k, v in train_features_labels.items():
         if int(k) == target_label:
             for diff_vec in v:
-                cnt += 1
                 dist = np.linalg.norm(diff_vec-target_feature)
                 if(min_dist is None or dist < min_dist):
                     min_dist = dist
-    return min_dist, cnt
+    return min_dist
 
 def compute_mulscore_mulvar(score, logvar, mulnum):
     vars = [np.exp(i) for i in logvar]
